306bf37d519a1a943df2e83be7b9cd1a9fc00e14
mrinaljain <jain.mrinal140@gmail.com>
2025-04-16 23:11:44 -0400

    experimental features added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,6 +61,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
         <GoogleAnalytics gaId="G-DY3RQS5E8M" />
 
