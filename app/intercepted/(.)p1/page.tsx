7e4fd6d13b07625574d0e7d4ec44d7beeb0ce418
mrinaljain <jain.mrinal140@gmail.com>
2025-04-22 13:32:20 -0400

    intercepted pages added

---
+++ app/intercepted/(.)p1/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function InterceptedP1() {
+  return (
+    <div>Intercepted Page P1 , yaha reffresh krne pr normal P! pr chala jayega</div>
+  )
+}
+
---
+++ app/intercepted/p1/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function P1Page() {
+  return (
+    <div>NORMAL P1 Page  </div>
+  )
+}
