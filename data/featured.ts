2565fe1e3ccbde6aad1f1feb66486e176c6eff7c
mrinaljain <jain.mrinal140@gmail.com>
2025-07-13 02:25:08 -0400

    featured section

--- data/featured.ts
+++ data/featured.ts
@@ -1,22 +1,27 @@
 export const featuredItems = [
    {
       name: "Shark Tank India",
-      logo: "/logos/sharktank.png",
-      url: "https://youtube.com/your-clip",
+      logo: "images/sharktank.png",
+      url: "https://www.linkedin.com/feed/update/urn:li:activity:7017113218225455104/",
    },
    {
-      name: "Forbes",
-      logo: "/logos/forbes.png",
-      url: "https://forbes.com/your-feature",
+      name: "30 Under 30",
+      logo: "images/forbes.png",
+      url: "https://www.youtube.com/watch?v=mNDLw2h6n4E",
    },
    {
-      name: "Economic Times",
-      logo: "/logos/et.png",
-      url: "https://economictimes.com/interview",
+      name: "Flutter Showcase",
+      logo: "images/flutter_showcase.svg",
+      url: "https://flutter.dev/showcase/stage#:~:text=%2D-,Mrinal,-%2C%20Flutter%20Lead%2C%20STAGE",
    },
    {
-      name: "YourStory",
-      logo: "/logos/yourstory.png",
-      url: "https://yourstory.com/story",
+      name: "Firebase Showcase",
+      logo: "images/firebase_showcase.svg",
+      url: "https://firebase.google.com/case-studies/stage-app#:~:text=%2D-,Mirnal,-%2C%20Flutter%20Lead%2C%20STAGE",
+   },
+   {
+      name: "Meta",
+      logo: "images/meta.svg",
+      url: "https://www.youtube.com/watch?v=GDDrs45PryY",
    },
 ];
