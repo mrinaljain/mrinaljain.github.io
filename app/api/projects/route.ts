9e38ff6e1718f9b902af61c322240df4c1f766c8
mrinaljain <jain.mrinlal140@gmail.com>
2026-01-28 03:04:29 -0500

    remove dbconnect old

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,14 +1,14 @@
-import dbConnect from "@/app/lib/db/connect";
-import Project from "@/app/lib/models/Project";
+import connectDB from "@/lib/db/mongodb";
+import Project from "@/models/Project";
 import { NextResponse } from "next/server";
 
-export  async function GET() {
-   await dbConnect();
+export async function GET() {
+   await connectDB();
    try {
       const projects = await Project.find({});
-       NextResponse.json(projects);
-   } catch (err:any) {
-      NextResponse.json({error:err.message})
+      NextResponse.json(projects);
+   } catch (err) {
+      NextResponse.json({ error: err })
    }
-   
+
 }
