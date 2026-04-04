08043f96ab6232cfc06b187740d03777627ce2fc
mrinaljain <jain.mrinal140@gmail.com>
2026-04-04 15:27:20 -0400

    build error fixed

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,20 +2,9 @@
 
 import { Metadata } from "next";
 import Script from "next/script";
-import Reffrences from "@/components/resume/Reffrences";
-import Hobbies from "@/components/resume/Hobbies";
-import Skills from "@/components/resume/Skills";
-import Education from "@/components/resume/Education";
-import Contact from "@/components/resume/Contact";
-import Volunteer from "@/components/resume/Volunteer";
-import Awards from "@/components/resume/Awards";
-import Experience from "@/components/resume/Experience";
-import "../../components/resume/Resume.css"
-import About from "@/components/resume/About";
-import DownloadResume from "@/components/resume/DownloadResume";
+import ResumeContent from "@/components/resume/ResumeContent";
 
 import { structuredResumeJsonLd } from "@/data/jsonLdResume";
-import Header from "@/components/resume/Header";
 import { createPageMetadata } from "@/lib/seo";
 
 export const metadata: Metadata = createPageMetadata({
@@ -34,30 +23,7 @@ export default function Resume() {
             dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredResumeJsonLd) }}
          />
 
-         <div className="body w-auto min-h-screen pt-1">
-            <div className="container mx-auto">
-               <div className="w-full flex flex-col">
-                  <Header />
-               </div>
-               <div className="flex flex-wrap">
-                  <div className="w-full sm:w-7/12 px-3.5 flex flex-col">
-                     <About />
-                     <Experience />
-                     <Awards />
-                     <Volunteer />
-                     {/* <Projects /> */}
-                  </div>
-                  <div className="w-full sm:w-5/12 px-3.5 flex flex-col">
-                     <Contact />
-                     <Education />
-                     <Skills />
-                     <Hobbies />
-                     <Reffrences />
-                  </div>
-               </div>
-            </div>
-            <DownloadResume />
-         </div>
+         <ResumeContent />
       </>
    );
 }
---
+++ src/components/resume/ResumeContent.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import "../../components/resume/Resume.css";
+import Reffrences from "@/components/resume/Reffrences";
+import Hobbies from "@/components/resume/Hobbies";
+import Skills from "@/components/resume/Skills";
+import Education from "@/components/resume/Education";
+import Contact from "@/components/resume/Contact";
+import Volunteer from "@/components/resume/Volunteer";
+import Awards from "@/components/resume/Awards";
+import Experience from "@/components/resume/Experience";
+import About from "@/components/resume/About";
+import DownloadResume from "@/components/resume/DownloadResume";
+import Header from "@/components/resume/Header";
+
+export default function ResumeContent() {
+  return (
+    <div className="body w-auto min-h-screen pt-1">
+      <div className="container mx-auto">
+        <div className="w-full flex flex-col">
+          <Header />
+        </div>
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-7/12 px-3.5 flex flex-col">
+            <About />
+            <Experience />
+            <Awards />
+            <Volunteer />
+            {/* <Projects /> */}
+          </div>
+          <div className="w-full sm:w-5/12 px-3.5 flex flex-col">
+            <Contact />
+            <Education />
+            <Skills />
+            <Hobbies />
+            <Reffrences />
+          </div>
+        </div>
+      </div>
+      <DownloadResume />
+    </div>
+  );
+}
